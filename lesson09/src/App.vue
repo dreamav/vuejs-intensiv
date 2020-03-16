f252df1a0d3662e590199099b32b4ac2f0d7e4a9
Mevericks <reloved@gmail.com>
2020-03-16 17:39:37 +0200

    [УРОК 6] init подготовка gulp версии (сборки) для vuex в будущем

---
+++ lesson09/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+	<div>
+		<header>
+			<div class="container">
+				<div class="row">
+					<div class="col col-sm-12">
+						<h1>Site</h1>
+						<app-cart></app-cart>
+					</div>
+				</div>
+			</div>
+		</header>
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col col-sm-12">
+						<app-product></app-product>
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+</template>
+
+<script>
+  import AppProduct from "./components/Product"
+  import AppCart from "./components/Cart"
+
+	export default {
+		components:{
+			AppProduct,
+			AppCart
+		}
+
+	}
+</script>
