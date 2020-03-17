1737e1d711773f2e586242c2b8c0e064656cbf8d
Mevericks <reloved@gmail.com>
2020-03-17 11:25:27 +0200

    [УРОК 6] деструктуризация ...mapMutations

--- lesson09/src/components/Product.vue
+++ lesson09/src/components/Product.vue
@@ -5,12 +5,13 @@
 			1000
 		</div>
 		<hr>
-		<button class="btn btn-warning" @click="onMinus">-1</button>
-		<button class="btn btn-success" @click="onPlus">+1</button>
+		<button class="btn btn-warning" @click="minus">-1</button>
+		<button class="btn btn-success" @click="plus">+1</button>
 	</div>
 </template>
 
 <script>
+  import {mapMutations} from "vuex";
 
 	export default {
 		data(){
@@ -19,12 +20,10 @@
 			}
 		},
 		methods:{
-			onMinus(){
-				this.$store.commit('minus')
-			},
-			onPlus(){
-				this.$store.commit('plus');
-			}
+			...mapMutations([
+        'plus',
+        'minus'
+      ]),
 		}
 	}
 </script>
