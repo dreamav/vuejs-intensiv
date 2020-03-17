b0feae886b5f43d54628b6c50251eaa20711c624
Mevericks <reloved@gmail.com>
2020-03-17 20:20:48 +0200

    [УРОК 7] добавил вывод товаров из модуля products

--- lesson11/src/components/ProductsList.vue
+++ lesson11/src/components/ProductsList.vue
@@ -1,12 +1,27 @@
 <template>
 	<div>
 		<h1>Products</h1>
+        <div class="row">
+            <div class="col col-sm-4"
+                 v-for="product in products"
+            >
+                <h3>{{ product.title }}</h3>
+                <div>{{ product.price }}</div>
+                <button class="btn btn-primary">Add to cart</button>
+                <button class="btn btn-warning">Remove from cart</button>
+            </div>
+        </div>
 	</div>
 </template>
 
 <script>
+    import {mapGetters} from "vuex";
 
 	export default {
-
+        computed:{
+            ...mapGetters('products',{
+                products: 'items'
+            })
+        }
 	}
 </script>
--- lesson11/src/store/modules/poroducts.js
+++ lesson11/src/store/modules/poroducts.js
@@ -1,10 +1,12 @@
 export default {
     namespaced: true,
     state:{
-        items: []
+        items: getProducts()
     },
     getters:{
-
+        items(state){
+            return state.items
+        }
     },
     mutations:{
 
@@ -13,3 +15,23 @@ export default {
 
     }
 };
+
+function getProducts(){
+    return [
+        {
+            id_product: 10,
+            title: 'Iphone 5',
+            price: 20000
+        },
+        {
+            id_product: 20,
+            title: 'Iphone 6',
+            price: 25000
+        },
+        {
+            id_product: 30,
+            title: 'Iphone 7',
+            price: 30000
+        }
+    ]
+}
