677f3a06a15c3b4d0a6dd5c86e8742900f6cd9e1
Mevericks <reloved@gmail.com>
2020-03-17 13:46:41 +0200

    [УРОК 7] init подготовка перед роутами

---
+++ lesson11/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+	<div>
+		<header>
+			<div class="container">
+				<div class="row">
+					<div class="col col-sm-9">
+						<h1>Site</h1>
+					</div>
+					<div class="col col-sm-3">
+						<div class="alert alert-default">
+							<div>In Cart: </div>
+						</div>
+					</div>
+				</div>
+				<hr>
+			</div>
+		</header>
+		<section>
+			<div class="container">
+				<div class="row">
+					<div class="col col-sm-3 menu">
+						<ul class="list-group">
+                            <li class="list-group-item">Products</li>
+                            <li class="list-group-item">Cart</li>
+                            <li class="list-group-item">Checkout</li>
+						</ul>
+					</div>
+					<div class="col col-sm-9">
+					</div>
+				</div>
+			</div>
+		</section>
+	</div>
+</template>
+
+<script>
+
+	export default {
+
+	}
+</script>
---
+++ lesson11/src/components/Cart.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<h1>Cart</h1>
+		<hr>
+	</div>
+</template>
+
+<script>
+
+	export default {
+
+	}
+</script>
