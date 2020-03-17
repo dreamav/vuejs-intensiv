f06e91f762de366ba21f510f78aeaea9afb1070a
Mevericks <reloved@gmail.com>
2020-03-17 19:47:03 +0200

    [УРОК 7] добавил название класса для active элемента

--- lesson11/src/App.vue
+++ lesson11/src/App.vue
@@ -26,6 +26,7 @@
                                 :to="item.url"
                                 tag="li"
                                 class="list-group-item"
+                                active-class="active"
                             >
                               <a>{{ item.text }}</a>
                             </router-link>
@@ -51,3 +52,54 @@
 
 	}
 </script>
+<style>
+    .menu{
+        border-right: 1px solid #ddd;
+    }
+
+    .list-group-item{
+        transition: background 0.3s, color 0.3s;
+    }
+
+    .list-group-item a{
+        text-decoration: none;
+    }
+
+    .list-group-item.active a{
+        color: inherit;
+    }
+
+    .slide-enter{
+
+    }
+
+    .slide-enter-active{
+        animation: slideIn 0.5s;
+    }
+
+    .slide-enter-to{
+
+    }
+
+    .slide-leave{
+
+    }
+
+    .slide-leave-active{
+        animation: slideOut 0.5s;
+    }
+
+    .slide-leave-to{
+
+    }
+
+    @keyframes slideIn{
+        from{transform: rotateY(90deg);}
+        to{transform: rotateY(0deg);}
+    }
+
+    @keyframes slideOut{
+        from{transform: rotateY(0deg);}
+        to{transform: rotateY(90deg);}
+    }
+</style>
