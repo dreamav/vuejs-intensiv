ef0ac2bcf1a769af050ca97008e962d988e2b225
Mevericks <reloved@gmail.com>
2020-03-17 22:08:35 +0200

    [УРОК 7] подключил vue-resource

--- lesson11/src/main.js
+++ lesson11/src/main.js
@@ -4,6 +4,11 @@ import App from './App.vue';
 import {store} from './store';
 import {router} from './routes';
 
+import VueResource from 'vue-resource';
+
+Vue.use(VueResource);
+Vue.http.options.root = 'http://js.dmitrylavrik.ru/api/'
+
 new Vue({
   el: '#app',
   store,
